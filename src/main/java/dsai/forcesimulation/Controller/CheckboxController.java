71dc04145718239e7453d5fb964a9d74a2e2fc38
kiet21042003 <tuankiet21042003@gmail.com>
2023-07-07 16:30:14 +0700

    update Checkbox

---
+++ src/main/java/dsai/forcesimulation/Controller/CheckboxController.java
@@ -0,0 +1,41 @@
+package dsai.forcesimulation.Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CheckboxController implements Initializable {
+
+    @FXML
+    private CheckBox forceBox, sumBox, valueBox, massBox, accelerateBox, veloBox, posiBox;
+
+    public boolean getForceBox(){
+        return forceBox.isSelected();
+    }
+    public boolean getSumBox(){
+        return sumBox.isSelected();
+    }
+    public boolean getValueBox(){
+        return valueBox.isSelected();
+    }
+    public boolean getMassBox(){
+        return massBox.isSelected();
+    }
+    public boolean getAccelerateBox(){
+        return accelerateBox.isSelected();
+    }
+    public boolean getVeloBox(){
+        return veloBox.isSelected();
+    }
+    public boolean getPosiBox(){
+        return posiBox.isSelected();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
